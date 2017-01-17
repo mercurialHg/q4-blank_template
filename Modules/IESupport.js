f3b0bd4c55e584510fa46b97c54de7a35f33bb28
Sustoid <jasonm@q4websystems.com>
2017-01-17 17:23:23 -0500

    Removed outdated project files.

--- Modules/IESupport.js
+++
@@ -1,5 +0,0 @@
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-	<script src="/files/bootstrap/js/html5shiv.js"></script>
-	<script src="/files/bootstrap/js/respond.min.js"></script>
-<![endif]-->
