daf3463a66b035a4dc368182243f8a2dd2c4c42e
Ionut Panait <pacalici@gmail.com>
2017-08-24 19:41:14 +0300

    updates

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'css'
+# images_dir = 'assets/images'
+# javascripts_dir = 'assets/javascripts'
+relative_assets = true
+line_comments = false
+# output_style = :compressed
